f5dee659e2d0bbd3070dccc2fc8073a309e27b7f
Nishchay Jain <nishchayjain.it20@jecrc.ac.in>
2026-01-27 21:44:45 +0530

    updated api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BACKEND_ENDPOINT = "https://qtify-backend-labs.crio.do";
+export const BACKEND_ENDPOINT = "https://qtify-backend.labs.crio.do";
 
 export const fetchTopAlbums = async () => {
   try {
